45a94ec8e414f6b42c2d430d2a7076759e726290
mpresill <26869915+mpresill@users.noreply.github.com>
2019-10-24 12:38:38 +0200

    Update auto_resubmit.sh

--- BSM3G_TNT_Maker/crab/auto_resubmit.sh
+++ BSM3G_TNT_Maker/crab/auto_resubmit.sh
@@ -1,5 +1,11 @@
 #Ensure you source crab and setup voms certificate
-for filename in Legacy*/crab_Legacy*/; do
+for filename in crab_projects_17/*; do
   echo $filename
   crab resubmit $filename
 done
+
+for filename_Data in Fall17V*/*; do
+  echo $filename_Data
+  crab resubmit $filename_Data
+done
+
