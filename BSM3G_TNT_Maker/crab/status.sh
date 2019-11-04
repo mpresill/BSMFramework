39fe6b18e08d8f1f5c2f45dfcce860536953d500
mpresill <26869915+mpresill@users.noreply.github.com>
2019-11-04 11:28:27 +0100

    Update status.sh

--- BSM3G_TNT_Maker/crab/status.sh
+++ BSM3G_TNT_Maker/crab/status.sh
@@ -11,28 +11,35 @@ crab status -d Legacy17V2_SMuBlockF/crab_Legacy17V2_SMuBlockF
 
 crab status -d crab_projects_17/crab_Fall17V2_DY1
 crab status -d crab_projects_17/crab_Fall17V2_DY2
+
 crab status -d crab_projects_17/crab_Fall17V2_ST
 crab status -d crab_projects_17/crab_Fall17V2_SaT
+
 crab status -d crab_projects_17/crab_Fall17V2_TT
+
+crab status -d crab_projects_17/crab_Fall17V2_WW
+crab status -d crab_projects_17/crab_Fall17V2_WZ
+crab status -d crab_projects_17/crab_Fall17V2_ZZ
+
+crab status -d crab_projects_17/crab_Fall17V2_WJetsHT100To200
+crab status -d crab_projects_17/crab_Fall17V2_WJetsHT200To400
 crab status -d crab_projects_17/crab_Fall17V2_WJetsHT400To600
 crab status -d crab_projects_17/crab_Fall17V2_WJetsHT600To800
 crab status -d crab_projects_17/crab_Fall17V2_WJetsHT800To1200
+crab status -d crab_projects_17/crab_Fall17V2_WJetsHT1200To2500
+crab status -d crab_projects_17/crab_Fall17V2_WJetsHT2500ToInf
+
 crab status -d crab_projects_17/crab_Fall17V2_WJets_1
 crab status -d crab_projects_17/crab_Fall17V2_WJets_2
-crab status -d crab_projects_17/crab_eejj_17_L13_M2000
+
 crab status -d crab_projects_17/crab_eejj_17_L13_M500
+crab status -d crab_projects_17/crab_eejj_17_L13_M1000
+crab status -d crab_projects_17/crab_eejj_17_L13_M2000
 crab status -d crab_projects_17/crab_eejj_17_L13_M5000
 crab status -d crab_projects_17/crab_eejj_17_L13_M8000
-crab status -d crab_projects_17/crab_mumujj_17_L13_M1000
-crab status -d crab_projects_17/crab_Fall17V2_WJetsHT100To200
-crab status -d crab_projects_17/crab_Fall17V2_WW
-crab status -d crab_projects_17/crab_mumujj_17_L13_M2000
-crab status -d crab_projects_17/crab_Fall17V2_WJetsHT1200To2500
-crab status -d crab_projects_17/crab_Fall17V2_WZ
+
 crab status -d crab_projects_17/crab_mumujj_17_L13_M500
-crab status -d crab_projects_17/crab_Fall17V2_WJetsHT200To400
-crab status -d crab_projects_17/crab_Fall17V2_ZZ
+crab status -d crab_projects_17/crab_mumujj_17_L13_M1000
+crab status -d crab_projects_17/crab_mumujj_17_L13_M2000
 crab status -d crab_projects_17/crab_mumujj_17_L13_M5000
-crab status -d crab_projects_17/crab_Fall17V2_WJetsHT2500ToInf
-crab status -d crab_projects_17/crab_eejj_17_L13_M1000
 crab status -d crab_projects_17/crab_mumujj_17_L13_M8000
